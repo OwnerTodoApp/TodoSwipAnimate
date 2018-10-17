ccef9f380a5d9f9c40199a1bbd3908fdc490ac0b
tongxing <tongxing@huami.com>
2018-10-17 17:30:20 +0800

    Initial commit

---
+++ TodoSwipAnimate/ViewController.h
@@ -0,0 +1,14 @@
+//  ViewController.h
+//  Created on 2018/10/17
+//  Description <#文件描述#>
+
+//  Copyright © 2018年 Huami inc. All rights reserved.
+//  @author tongxing(tongxing@huami.com)
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TodoSwipAnimateTests/TodoSwipAnimateTests.m
@@ -0,0 +1,36 @@
+//  TodoSwipAnimateTests.m
+//  Created on 2018/10/17
+//  Description <#文件描述#>
+
+//  Copyright © 2018年 Huami inc. All rights reserved.
+//  @author tongxing(tongxing@huami.com)
+
+#import <XCTest/XCTest.h>
+
+@interface TodoSwipAnimateTests : XCTestCase
+
+@end
+
+@implementation TodoSwipAnimateTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
